63b7ba404d1beff80b1ac1be34484c07556b231a
miesho <marcin.ryzewskii@gmail.com>
2025-06-08 21:49:57 +0200

    Add gitsings to nvim config

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -47,7 +47,11 @@ return {
           "MunifTanjim/nui.nvim",
           -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
         }
-    } 
+    },
+
+    {
+        "lewis6991/gitsigns.nvim"
+    }
 }
 
 
